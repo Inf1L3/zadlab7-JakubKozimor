dd07e560f91c4c175f53fefbd9b0b0686d93aaee
Jakub <Jakub@GUZIK>
2018-04-24 23:01:02 +0200

    Zadanie  zrobione

--- OOPL7/src/main/java/pl/edu/ur/oopl7/Main.java
+++ OOPL7/src/main/java/pl/edu/ur/oopl7/Main.java
@@ -19,7 +19,46 @@ public class Main {
         System.out.println(st.toString());
         Wykladowca w = new Wykladowca(0, 0, " ", " ", " ", 0, " ");
         System.out.println(w.toString());
+        Punkt2D[] array2D = new Punkt2D[100];
+          Punkt3D[] array3D = new Punkt3D[100];
 
+          
+          
+           for(int i=0; i<100; i++)
+          {
+              array2D[i] = new Punkt2D();
+        
+             
+          }
+           
+             for(int i=0; i<100; i++)
+          {
+              array3D[i] = new Punkt3D();
+           
+          }
+          
+          for(int i=0; i<100; i++)
+          {
+              array2D[i].losuj();
+           
+             
+          }
+
+           for(int i=0; i<100; i++)
+          {
+              array3D[i].losuj();
+             
+          }
+
+            for(int i=0; i<100; i++)
+          {
+              if(array2D[i].x == array3D[i].x && array2D[i].y == array3D[i].y)
+                {
+                  
+                    System.out.println(array2D[i].toString());
+                  System.out.println(array3D[i].toString());
+                }
+          }
     }
 
 }
